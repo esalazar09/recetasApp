20471f495468ef40a0d6d8847c6f5679dbad02fa
esalazar09 <esalazar09@hotmail.com>
2017-03-10 08:56:24 -0600

    Commit 2
    recipe-detail, recipe, ingredients

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component'
     HeaderComponent,
     RecipeComponent,
     RecipeListComponent,
+    RecipeDetailComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
+
+  @Input() recipe : Recipe;
+  recipeId: number;
 
   constructor() { }
 
